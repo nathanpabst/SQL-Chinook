78252cb5c7fd650cf4c1c90d5c54a468bc47db14
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-16 19:31:08 -0500

    ex two complete

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,7 +1,12 @@
---non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
-select
+--2) brazil_customers.sql: Provide a query only showing the Customers from Brazil.
+select *
+from Customer
+where Country = 'Brazil'
+
+--1) non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+/*select
 	FullName = FirstName + ' ' + LastName,
 	CustomerId,
 	Country
 from Customer
-where Country != 'USA'
+where Country != 'USA'*/
