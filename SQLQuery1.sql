c9ce29a666576de76c601fa4eddd23a457d625cb
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-16 19:20:09 -0500

    ex 1 complete

---
+++ SQLQuery1.sql
@@ -0,0 +1,7 @@
+--non_usa_customers.sql: Provide a query showing Customers (just their full names, customer ID and country) who are not in the US.
+select
+	FullName = FirstName + ' ' + LastName,
+	CustomerId,
+	Country
+from Customer
+where Country != 'USA'
