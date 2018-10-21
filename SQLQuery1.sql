b860deb1ac6c08a2a229a6b254222cd62c58e96b
Nathan Pabst <nathan.pabst@gmail.com>
2018-10-20 19:32:49 -0500

    ex 25 complete

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,14 @@
+--25) top_5_tracks.sql: Provide a query that shows the top 5 most purchased songs.
+
+select top 5
+	[Song Name] = t.Name,
+	Purchases = sum(l.Quantity)
+from InvoiceLine as l
+	join Track as t on t.TrackId = l.TrackId
+	join Invoice as i on i.InvoiceId = l.InvoiceId
+group by t.Name
+order by Purchases desc
+
 --24) top_2013_track.sql: Provide a query that shows the most purchased track of 2013.
 
 select top 1
